64d251a3f39c45f56ce1679c9a54d7418148a689
Joseph Trubenstein <josephtrubenstein@gmail.com>
2022-06-07 09:56:01 +0900

    second

--- src/components/CnftFeed.js
+++ src/components/CnftFeed.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react"
-import { graphql, useStaticQuery } from "gatsby"
 
 const CnftFeed = () => {
   // Client-side Runtime Data Fetching
@@ -7,7 +6,7 @@ const CnftFeed = () => {
   const [cnftList, setCnftList] = useState(0)
   useEffect(() => {
     // get data from GitHub api
-    fetch(`https://api.opencnft.io/1/rank`)
+    fetch(`https://api.opencnft.io/1/rank?window=24h`)
       .then(response => response.json()) // parse JSON from request
       .then(resultData => {
         setCnftList(resultData.ranking)
@@ -35,14 +34,14 @@ const CnftFeed = () => {
         <div className="flex flex-wrap -m-4">
           {cnftList ? (
             cnftList.slice(0, 6).map(cnft => (
-              <div key={cnft.name} className="p-4 md:w-1/3">
-                <div className="h-full border-2  border-bubble-gum border-opacity-60 rounded-lg overflow-hidden">
-                  {cnft.thumbnail_type == "video/mp4" ? (
+              <div key={cnft.name} className="p-4 md:w-1/3 w-full">
+                <div className="h-full border-2 border-bubble-gum border-opacity-60 rounded-lg overflow-hidden">
+                  {cnft.thumbnail_type === "video/mp4" ? (
                     <video
                       autoPlay
                       loop
                       muted
-                      className="lg:h-48 md:h-36 w-full h-56 object-cover object-center"
+                      className="lg:h-64 md:h-36 w-full h-56 object-cover object-center"
                     >
                       <source
                         src={"https://ipfs.io/ipfs/" + cnft.thumbnail.slice(7)}
@@ -52,26 +51,28 @@ const CnftFeed = () => {
                     </video>
                   ) : (
                     <img
-                      className="lg:h-48 md:h-36 w-full h-56 object-cover object-center"
+                      className="lg:h-64 md:h-36 w-full h-56 object-cover object-center"
                       src={"https://ipfs.io/ipfs/" + cnft.thumbnail.slice(7)}
                       alt="blog"
                     />
                   )}
 
                   <div className="p-6">
-                    <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
-                      CATEGORY
-                    </h2>
+                    {/* <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
+                      
+                    </h2> */}
                     <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
                       {cnft.name}
                     </h1>
-                    <p className="leading-relaxed mb-3">
-                      Photo booth fam kinfolk cold-pressed sriracha leggings
-                      jianbing microdosing tousled waistcoat.
-                    </p>
+                    {/* <h2 className="leading-relaxed mb-3">
+                     {cnft.total_assets}
+                    </h2> */}
                     <div className="flex items-center flex-wrap ">
-                      <a href={cnft.link} className="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">
-                        Learn More
+                      <a
+                        href={cnft.link}
+                        className="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0"
+                      >
+                       See on Open CNFT
                         <svg
                           className="w-4 h-4 ml-2"
                           viewBox="0 0 24 24"
@@ -86,7 +87,7 @@ const CnftFeed = () => {
                         </svg>
                       </a>
                       <span className="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
-                        <svg
+                        {/* <svg
                           className="w-4 h-4 mr-1"
                           stroke="currentColor"
                           strokeWidth={2}
@@ -97,11 +98,11 @@ const CnftFeed = () => {
                         >
                           <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z" />
                           <circle cx={12} cy={12} r={3} />
-                        </svg>
-                        1.2K
+                        </svg> */}
+                        {cnft.total_minted}
                       </span>
                       <span className="text-gray-400 inline-flex items-center leading-none text-sm">
-                        <svg
+                        {/* <svg
                           className="w-4 h-4 mr-1"
                           stroke="currentColor"
                           strokeWidth={2}
@@ -111,8 +112,8 @@ const CnftFeed = () => {
                           viewBox="0 0 24 24"
                         >
                           <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z" />
-                        </svg>
-                        6
+                        </svg> */}
+                        minted
                       </span>
                     </div>
                   </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react"
 import Layout from "../components/layout"
 import SeoWrapper from "../components/SeoWrapper"
 import Hero from "../components/Hero"
-// import Blog from "../components/Blog"
+import Blog from "../components/Blog"
 import Speakers from "../components/Speakers"
 import SummitFeatures from "../components/SummitFeatures"
 import CnftFeed from "../components/CnftFeed"
@@ -15,7 +15,7 @@ const IndexPage = () => (
       <Speakers />
       <SummitFeatures />
       <CnftFeed />
-      {/* <Blog /> */}
+      <Blog />
     </Layout>
   </div>
 )
